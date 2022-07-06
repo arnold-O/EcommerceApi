2c35e5659e38d94f86a3aa148794e980109c07d5
Arnold-O <arnoldoghiator@gmail.com>
2022-07-06 23:29:10 +0100

    setup complete

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.use("/api/v1/products", ProductRoutes);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.MONGO_URI;
+
+mongoose.connect(DB, {}).then((conn) => {
+  console.log(conn.connection);
+});
+
+const PORT = process.env.PORT || 5500;
+
+app.listen(PORT, () => {
+  console.log(`port running on ${PORT}`);
+});
+
+
