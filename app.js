f04aa5f892e42a67376b17c4c89371acdcc93afd
Arnold-O <arnoldoghiator@gmail.com>
2022-07-07 23:51:34 +0100

    global error setup

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const express = require("express")
-const ProductRoutes = require('./route/productRoutes')
-const AuthRoutes = require('./route/authRoute')
-const errorHandler = require('./middlewares/errorHandler')
+const express = require("express");
 
-const app = express()
-const morgan = require('morgan')
+const errorHandler = require("./middlewares/errorHandler");
 
-app.use(express.json())
-app.use(morgan('tiny'))
+// all route section begins
+const ProductRoutes = require("./route/productRoutes");
+const AuthRoutes = require("./route/authRoute");
 
+const app = express();
+const morgan = require("morgan");
 
-
-
-
+app.use(express.json());
+app.use(morgan("tiny"));
 
 
 
+// routes middlewares
 app.use("/api/v1/auth", AuthRoutes);
 app.use("/api/v1/product", ProductRoutes);
 
-app.use(errorHandler)
+app.use(errorHandler);
 module.exports = app;
